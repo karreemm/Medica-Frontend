3f608facab3a713699e479b21b993f6213ec7638
kareem <kareem.abdelnabi.22@gmail.com>
2024-07-29 21:48:25 +0300

    fix3

--- app/tailwindcss-lib-util-flattenColorPalette.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "tailwindcss/lib/util/flattenColorPalette" {
-  // Add the necessary type declarations here
-  // For example, if the module exports a function, you can declare it like this:
-  function flattenColorPalette(colors: any): any;
-
-  export = flattenColorPalette;
-}
