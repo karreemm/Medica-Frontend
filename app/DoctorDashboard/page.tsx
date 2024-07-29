12b866739e28657cc9b7e24b6411f0ee714c1c39
kareem <kareem.abdelnabi.22@gmail.com>
2024-07-30 00:30:55 +0300

    fix6

--- app/DoctorDashboard/page.tsx
+++ app/DoctorDashboard/page.tsx
@@ -1,20 +1,37 @@
 "use client";
+import { useState, useEffect } from "react";
 import Sidebar from "./Sidebar";
 import NavbarUser from "../Components/NavbarUser";
-import Requests  from "./Requests";
-import { useState } from "react";
-import  Appointments  from "./Appointments";
-import Scans  from "./Scans";
+import Requests from "./Requests";
+import Appointments from "./Appointments";
+import Scans from "./Scans";
 import { Surgeries } from "./Surgeries";
 import Footer from "../Components/Footer";
 
 export default function Page() {
+  const [userObj, setUserObj] = useState<any>(null);
+  const [activeComponent, setActiveComponent] = useState<string>('Appointments');
 
-  
-  const [activeComponent, setActiveComponent] = useState('Appointments');
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      // Safely access localStorage
+      const user = localStorage.getItem("User");
+      if (user) {
+        const parsedUser = JSON.parse(user);
+        setUserObj(parsedUser);
+        if (!parsedUser || parsedUser.role !== "nurse") {
+          window.location.href = "/login";
+        }
+      } else {
+        window.location.href = "/login";
+      }
+    }
+  }, []);
+
   const handleSidebarOptionClick = (option: string) => {
     setActiveComponent(option);
   };
+
   const renderComponent = () => {
     switch (activeComponent) {
       case 'Requests':
@@ -25,20 +42,26 @@ export default function Page() {
         return <Scans />;
       case 'Surgeries':
         return <Surgeries />;
+      default:
+        return <Appointments />;
     }
   };
+
+  // Render nothing while userObj is null
+  if (!userObj) {
+    return null;
+  }
 
   return (
     <>
       <div className="bg-[#669bbc]">
-        {/* <NavbarComponent /> */}
         <NavbarUser />
         <div className="flex flex-col items-center md:flex-row md:justify-center md:items-start mx-5 mt-32 md:space-x-10">
           <div className="flex flex-row items-center md:basis-[12%] w-full">
-            <Sidebar  onOptionClick={handleSidebarOptionClick}/>
+            <Sidebar onOptionClick={handleSidebarOptionClick} />
           </div>
           <div className="md:basis-[70%] w-full mt-5 md:mt-0 h-[570px]">
-          {renderComponent()}
+            {renderComponent()}
           </div>
         </div>
       </div>
