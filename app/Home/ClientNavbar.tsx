412c1824459b3f270204674db97290df63408a41
kareem <kareem.abdelnabi.22@gmail.com>
2024-07-30 14:36:26 +0300

    fix8

--- app/Home/ClientNavbar.tsx
+++ app/Home/ClientNavbar.tsx
@@ -6,13 +6,11 @@ const ClientNavbar: React.FC = () => {
   const [NavbarComponent, setNavbarComponent] = useState<React.ElementType>(() => Navbar); // Default to Navbar
 
   useEffect(() => {
-    if (typeof window !== "undefined") {
       const user = localStorage.getItem("User");
       if (user) {
         const userObj = JSON.parse(user);
         if (userObj) {
           setNavbarComponent(() => NavbarUser); // Use NavbarUser if user exists
-        }
       }
     }
   }, []); // Empty dependency array means this runs once on component mount
